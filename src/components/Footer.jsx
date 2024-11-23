4469ab00db4d44d1304ffd57993e173b687c40ab
Zeeshanx01 <zeeshanmunir13579@gmail.com>
2024-11-23 14:14:53 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,14 @@ const Footer = () => {
   return (
     <div className='bg-slate-800 h-[6vh] min-h-16 flex flex-col justify-center items-center text-white fixed bottom-0 w-full'>
 
-
-      <div className="logo font-bold text-white text-2xl">
-        <span className='text-green-700'>&lt;</span>
-        Pass
-        <span className='text-green-700'>OP/&gt;</span>
+<div className='flex justify-center items-center'>
+        <div className="logo font-bold text-white text-2xl">
+          <span className='text-green-700'>&lt;</span>
+          Pass
+          <span className='text-green-700'>OP/&gt;</span>
+        </div>
+        <p className='mx-1 text-xs'> [Local]</p>
       </div>
-      
       <h1>Created by Zeeshan-x01</h1>
 
     </div>
